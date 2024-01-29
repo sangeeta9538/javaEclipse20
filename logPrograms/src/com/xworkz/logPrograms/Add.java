70ac8414308ea51025c925f90919e9561574e7d0
java95 <sangeetak.xworkz@gmail.com>
2024-01-29 19:35:20 +0530

    ex

---
+++ logPrograms/src/com/xworkz/logPrograms/Add.java
@@ -0,0 +1,13 @@
+package com.xworkz.logPrograms;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10;
+        int b=10;
+        int c=(a+b);
+        System.out.println(c);
+	}
+
+}
